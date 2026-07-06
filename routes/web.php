dd8f8a6e3fb43774361e836f3292294efb913106
bunny <rabbani.hibban@gmail.com>
2026-07-06 20:05:56 +0700

    Menambahkan konfigurasi vercel untuk Front-end

--- routes/web.php
+++ routes/web.php
@@ -3,8 +3,6 @@ use Illuminate\Support\Facades\Route;
 use Illuminate\Http\Request;
 use App\Http\Controllers\AuthController;
 
-// Rute ini akan menangkap semua URL dan menampilkannya melalui view 'app',
-// menyerahkan kontrol routing selanjutnya kepada React Router.
 Route::get('/{any?}', function () {
     return view('app');
 })->where('any', '.*');
